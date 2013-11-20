7e3cc70b3ea3a8bf0d1fd78fd0389d376c7c9f9c
linbinbin <linbinbin90@gmail.com>
2013-11-20 18:38:45 -0500

    Version 1 Have problem in InitializeServlet.java of cloudfront

--- workspace/MyYouTube/WebContent/upload.jsp
+++ workspace/MyYouTube/WebContent/upload.jsp
@@ -5,13 +5,37 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
 <title>MyYouTube: Upload your video</title>
+<script language="JavaScript" type="text/JavaScript">   
+	var  img=null;   
+	function checkPic(upload){   
+		var location=upload.video.value;   
+		if(location==""){   
+			alert("please select a video");   
+			return false;   
+		}   
+		var point = location.lastIndexOf(".");   
+		var type = location.substr(point);   
+		if(type==".mp4"||type==".rmvb"){
+			return true;   
+		}   
+		else{   
+			alert("format is not acceptable");   
+			return false;   
+		}   
+		return true;   
+	}   
+	
+	function changesrc(){   
+		yourpic.src=picForm.pic.value;   
+	}   
+</script>  
 </head>
 <body>
 
 
-<form method="POST" enctype="multipart/form-data" action="./ServletUpload">
-  File to upload: <input type="file" name="upfile"><br/>
-  Notes about the file: <input type="text" name="note"><br/>
+<form name="upload" method="POST" enctype="multipart/form-data" action="./ServletUpload" onsubmit="return checkPic(upload);">
+  File to upload: <input type="file" name="upfile" id="video"><br/>
+  <!--  Notes about the file: <input type="text" name="note"><br/>  -->
   <br/>
   <input type="submit" value="Press"> to upload the file!
 </form>
--- workspace/MyYouTube/src/com/jsp/MyYouTube/InitializeServlet.java
+++ workspace/MyYouTube/src/com/jsp/MyYouTube/InitializeServlet.java
@@ -1,17 +1,32 @@
 package com.jsp.MyYouTube;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.cloudfront.AmazonCloudFrontClient;
+import com.amazonaws.services.cloudfront.model.CreateDistributionRequest;
+import com.amazonaws.services.cloudfront.model.CreateStreamingDistributionRequest;
+import com.amazonaws.services.cloudfront.model.DistributionConfig;
+import com.amazonaws.services.cloudfront.model.Origin;
+import com.amazonaws.services.cloudfront.model.Origins;
+import com.amazonaws.services.cloudfront.model.S3Origin;
+import com.amazonaws.services.cloudfront.model.S3OriginConfig;
+import com.amazonaws.services.cloudfront.model.StreamingDistributionConfig;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 /**
  * Servlet implementation class InitializeServlet
  */
 public class InitializeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private final String BUCKET_NAME = "cloudcompute";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,11 +43,50 @@ public class InitializeServlet extends HttpServlet {
 
 	@Override
 	public void init() throws ServletException {
-		// TODO Auto-generated method stub
+		AWSCredentialsProvider credentialsProvider = new ClasspathPropertiesFileCredentialsProvider();
+		AmazonCloudFrontClient cloudfront = new AmazonCloudFrontClient(credentialsProvider);
 		
+		System.out.println("initial the website");
+		
+		ServletContext application = this.getServletContext();
 		//create s3
-		
+		AmazonS3Manager manager = new AmazonS3Manager();
+		try {
+			manager.createS3Bucket();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//combine distribution
+		
+//		S3Origin origin = new S3Origin()
+//        .withDomainName(BUCKET_NAME+"s3.amazonaws.com");
+//
+//        S3Origins origins = new Origins().withItems(items).withItems(origin);
+//
+//        DistributionConfig streamingDistributionConfig1 = new DistributionConfig()
+//        .withCallerReference("unique-id-for-idempotency")
+//        .withComment("Streaming CloudFront distribution")
+//        .withDefaultRootObject("index.html")
+//        .withEnabled(true)
+//        .withOrigins(origins); 
+			
+		StreamingDistributionConfig streamingDistributionConfig = new StreamingDistributionConfig()
+		.withCallerReference("unique-id-for-idempotency")
+        .withComment("Streaming CloudFront distribution")
+        .withEnabled(true);
+		//include the with parameters 
+		CreateStreamingDistributionRequest streamingDistribution = new CreateStreamingDistributionRequest()
+		        .withStreamingDistributionConfig(streamingDistributionConfig);          
+		cloudfront.createStreamingDistribution(streamingDistribution);
+		
+		
+		
+		
+		DistributionConfig downloadingDistributionConfig = new DistributionConfig();
+		CreateDistributionRequest downloadingDistribution = new CreateDistributionRequest()
+		        .withDistributionConfig(downloadingDistributionConfig); 
+		cloudfront.createDistribution(downloadingDistribution);
 		super.init();
 	}
 
